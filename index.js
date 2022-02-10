35adb466ce57820bb9b2dd7ee23bb92d97d12719
Matheus Alves <malves224@gmail.com>
2022-02-10 20:16:25 -0300

    mudando response

--- index.js
+++ index.js
@@ -4,6 +4,6 @@ require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.get('/', (req, res) => res.send(`<h1> Executando na porta: ${port} </h1>`));
+app.get('/', (req, res) => res.send('<h1> o Bicho ta vivo </h1>'));
 
 app.listen(port, () => console.log(`Rodando na porta: ${port}`));
